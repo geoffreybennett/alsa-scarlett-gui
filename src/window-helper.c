9c64dc1fed246f91d8fa6d4bcfb82eb7b9d81f30
Geoffrey D. Bennett <g@b4.vu>
2024-02-23 03:42:56 +1030

    Make window-helper.c close the window when Esc is pressed
    Fixes #102.

--- src/window-helper.c
+++ src/window-helper.c
@@ -12,6 +12,25 @@ gboolean window_startup_close_request(GtkWindow *w, gpointer data) {
   return true;
 }
 
+static gboolean on_key_press(
+  GtkEventControllerKey *controller,
+  guint                  keyval,
+  guint                  keycode,
+  GdkModifierType        state,
+  gpointer               user_data
+) {
+  GtkWidget *widget = gtk_event_controller_get_widget(
+    GTK_EVENT_CONTROLLER(controller)
+  );
+
+  if (keyval == GDK_KEY_Escape) {
+    gtk_window_close(GTK_WINDOW(widget));
+    return 1;
+  }
+
+  return 0;
+}
+
 GtkWidget *create_subwindow(
   struct alsa_card *card,
   const char       *name,
@@ -23,6 +42,12 @@ GtkWidget *create_subwindow(
   gtk_window_set_resizable(GTK_WINDOW(w), FALSE);
   gtk_window_set_title(GTK_WINDOW(w), title);
   g_signal_connect(w, "close_request", G_CALLBACK(close_callback), card);
+
+  GtkEventController *key_controller = gtk_event_controller_key_new();
+  gtk_widget_add_controller(w, key_controller);
+  g_signal_connect(
+    key_controller, "key-pressed", G_CALLBACK(on_key_press), NULL
+  );
 
   g_free(title);
   return w;
